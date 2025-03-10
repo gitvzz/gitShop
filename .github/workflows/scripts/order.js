f553ab0469ea2106f7e1943c0194872e1d196368
gitvzz <gitvz.me@gmail.com>
2025-03-11 03:06:14 +0800

    1

--- .github/workflows/scripts/order.js
+++ .github/workflows/scripts/order.js
@@ -1,13 +1,26 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
-exports.OrderProcessor = void 0;
+exports.OrderAction = void 0;
 const issue_action_1 = require("./base/issue-action");
 /**
  * 订单处理
  */
-class OrderProcessor extends issue_action_1.IssueAction {
+class OrderAction extends issue_action_1.IssueAction {
     async execute() {
-        // 实现订单处理逻辑
+        try {
+            this.log('开始处理新订单...');
+            console.log(this.context.issue);
+        }
+        catch (error) {
+            this.fail(`处理新订单时出错: ${error instanceof Error ? error.message : String(error)}`);
+        }
     }
 }
-exports.OrderProcessor = OrderProcessor;
+exports.OrderAction = OrderAction;
+if (require.main === module) {
+    const action = new OrderAction();
+    action.execute().catch(error => {
+        console.error('Action执行失败:', error);
+        process.exit(1);
+    });
+}
--- distributors/README.md
+++ distributors/README.md
@@ -6,7 +6,8 @@ Fork 项目后，按以下格式添加文件
 {
     "id": 97074821,
     "username": "netferes",
-    "full_name": "https://github.com/netferes/gitShop11",
+    "repo":"gitShop11",
+    "url": "https://github.com/netferes/gitShop11",
     "address": "0x44E05b79817b4A1829c1aC8598F3416a93666666"
 }
 ```
